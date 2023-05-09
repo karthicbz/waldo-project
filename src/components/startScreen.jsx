130afea58d26b4789b874b5738e59bde8ae12d67
Karthick <pkarthicbz@live.in>
2023-05-09 13:42:35 +0530

    add elements into start screen

--- src/components/startScreen.jsx
+++ src/components/startScreen.jsx
@@ -1,7 +1,18 @@
 const StartScreen = ()=>{
+    const modes = ['Easy', 'Medium', 'Hard'];
     return(
         <div>
-            
+            <h1>Where's Waldo?</h1>
+            <p>Please refer this <a href="https://waldo.fandom.com/wiki/Where%27s_Waldo%3F">link</a> for more info about the game.</p>
+            <p>Choose a game</p>
+            {modes.map(mode=>{
+                return(
+                    <div>
+                        <img src="#" alt="dummy"/>
+                        <p>{mode}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
