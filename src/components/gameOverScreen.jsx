559edc69756f897ec8982bcc33c14d2576df4def
karthicbz <pkarthicbz@live.in>
2023-05-10 10:27:02 +0530

    store game scores into firestore

--- src/components/gameOverScreen.jsx
+++ src/components/gameOverScreen.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 const GameOverScreen = ({time, params})=>{
     // console.log(params);
-    const [scores, setScores] = useState([]);
+    const [scores, setScores] = useState(undefined);
     const currentGame = Object.values(params)[0];
 
     async function getScoreData(){
@@ -17,10 +17,31 @@ const GameOverScreen = ({time, params})=>{
             tempScores = [...tempScores, {id:snap.id, data:snap.data()}];
         });
         tempScores.forEach(score=>{
-            if(score.id === 'gameone'){
-                setScores(score.data);
+            if(score.id === currentGame){
+                setScores(score.data.score);
             }
         })
+    }
+
+    async function addScoreData(){
+        const db = getFirestore(app);
+        const docRef = doc(db, "score", currentGame);
+        try{
+            await updateDoc(docRef, {
+                score: scores,
+            })
+        }catch(e){
+            console.log('error adding data', e);
+        }
+    }
+
+    function saveData(){
+        const playerName = document.querySelector('.player-name-input');
+        if(scores !== undefined){
+            setScores([...scores, {name:playerName.value, time:time}]);
+        }else{
+            setScores([{name:playerName.value, time:time}]);
+        }
     }
 
     useEffect(()=>{
@@ -28,35 +49,17 @@ const GameOverScreen = ({time, params})=>{
         console.log(scores);
     }, [])
 
-    async function addScoreData(name, time){
-        const db = getFirestore(app);
-        const docRef = doc(db, "score", currentGame);
-        try{
-            // const docRef = await addDoc(collection(db, "score"), {
-            //     name: name,
-            //     time: time,
-            // });
-            await updateDoc(docRef, {
-                score: [{name: name,
-                time: time,}]
-            })
-        }catch(e){
-            console.log('error adding data', e);
+    useEffect(()=>{
+        if(scores !== undefined){
+            addScoreData();
         }
-    }
-
-
-
-    function saveData(){
-        const playerName = document.querySelector('.player-name-input');
-        addScoreData(playerName.value, time);
-    }
+    }, [scores])
 
     return(
         <div className="game-over-screen">
             <p>You completed the game in {time}</p>
             <input type="text" placeholder="Your name.." className="player-name-input"/>
-            <button onClick={saveData}>Save</button>
+            <button onClick={saveData} className="save-score">Save</button>
             <Link to={"/"}><button>Home</button></Link>
         </div>
     )
